fb4341eac2cd70367a83b8823060c5c27a6af5e1
pushpa446 <pdumpala@thoughtworks.com>
2016-09-10 15:06:43 +0530

    changed the names

--- bed_management.sql
+++ bed_management.sql
@@ -10,43 +10,43 @@ values ( 'ICU','This is an Intensive Care Unit for patients that are in critical
 insert into location_tag_map ( location_id, location_tag_id )
 values ((select location_id from location where name = 'ICU'), (select location_tag_id from location_tag where name = 'Admission Location'));
 insert into location ( name, description, creator, date_created, uuid, parent_location)
-values ( 'Physical Space for ICU','Physical space','1',now(), uuid(),
+values ( 'ICU ward','Physical space','1',now(), uuid(),
 (SELECT id FROM
 			(SELECT location_id as id FROM location where name = 'ICU')
 			AS tmptable)
 );
 
 insert into location_tag_map ( location_id, location_tag_id )
-values ((select location_id from location where name = 'Physical Space for ICU'), (select location_tag_id from location_tag where name = 'Admission Location'));
+values ((select location_id from location where name = 'ICU ward'), (select location_tag_id from location_tag where name = 'Admission Location'));
 
 insert into bed (bed_number, bed_type_id, date_created, uuid) values ('ICU1', 102, now(), uuid());
 insert into bed (bed_number, bed_type_id, date_created, uuid) values ('ICU2', 102, now(), uuid());
 insert into bed (bed_number, bed_type_id, date_created, uuid) values ('ICU3', 102, now(), uuid());
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for ICU'),'1','1',(select bed_id from bed where bed_number = 'ICU1'));
+values ((select location_id from location where name = 'ICU ward'),'1','1',(select bed_id from bed where bed_number = 'ICU1'));
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for ICU'),'1','2',(select bed_id from bed where bed_number = 'ICU2'));
+values ((select location_id from location where name = 'ICU ward'),'1','2',(select bed_id from bed where bed_number = 'ICU2'));
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for ICU'),'1','3',(select bed_id from bed where bed_number = 'ICU3'));
+values ((select location_id from location where name = 'ICU ward'),'1','3',(select bed_id from bed where bed_number = 'ICU3'));
 
 
 
 insert into location ( name, description, creator, date_created, uuid )
-values ( 'General Ward','This is for the admitted patients that need special care.','1', now(), uuid() );
+values ( 'General','This is for the admitted patients that General Ward special care.','1', now(), uuid() );
 
 insert into location_tag_map ( location_id, location_tag_id )
-values ((select location_id from location where name = 'General Ward'), (select location_tag_id from location_tag where name = 'Admission Location'));
+values ((select location_id from location where name = 'General'), (select location_tag_id from location_tag where name = 'Admission Location'));
 
 insert into location ( name, description, creator, date_created, uuid, parent_location)
-values ( 'Physical Space for General Ward','Physical space','1',now(), uuid(),
+values ( 'General Ward','Physical space','1',now(), uuid(),
 (SELECT id FROM
-			(SELECT location_id as id FROM location where name = 'General Ward')
+			(SELECT location_id as id FROM location where name = 'General')
 			AS tmptable)
 );
 
 insert into location_tag_map ( location_id, location_tag_id )
-values ((select location_id from location where name = 'Physical Space for General Ward'), (select location_tag_id from location_tag where name = 'Admission Location'));
+values ((select location_id from location where name = 'General Ward'), (select location_tag_id from location_tag where name = 'Admission Location'));
 
 
 insert into bed (bed_number, bed_type_id) values ('GM 1', 101);
@@ -62,31 +62,31 @@ insert into bed (bed_number, bed_type_id) values ('GM 10', 101);
 
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for General Ward'),'1','1',(select bed_id from bed where bed_number = 'GM 1'));
+values ((select location_id from location where name = 'General Ward'),'1','1',(select bed_id from bed where bed_number = 'GM 1'));
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for General Ward'),'1','2',(select bed_id from bed where bed_number = 'GM 2'));
+values ((select location_id from location where name = 'General Ward'),'1','2',(select bed_id from bed where bed_number = 'GM 2'));
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for General Ward'),'1','3',(select bed_id from bed where bed_number = 'GM 3'));
+values ((select location_id from location where name = 'General Ward'),'1','3',(select bed_id from bed where bed_number = 'GM 3'));
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for General Ward'),'1','4',(select bed_id from bed where bed_number = 'GM 4'));
+values ((select location_id from location where name = 'General Ward'),'1','4',(select bed_id from bed where bed_number = 'GM 4'));
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for General Ward'),'2','1',(select bed_id from bed where bed_number = 'GM 5'));
+values ((select location_id from location where name = 'General Ward'),'2','1',(select bed_id from bed where bed_number = 'GM 5'));
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for General Ward'),'2','2',(select bed_id from bed where bed_number = 'GM 6'));
+values ((select location_id from location where name = 'General Ward'),'2','2',(select bed_id from bed where bed_number = 'GM 6'));
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for General Ward'),'2','3',(select bed_id from bed where bed_number = 'GM 7'));
+values ((select location_id from location where name = 'General Ward'),'2','3',(select bed_id from bed where bed_number = 'GM 7'));
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for General Ward'),'2','4',(select bed_id from bed where bed_number = 'GM 8'));
+values ((select location_id from location where name = 'General Ward'),'2','4',(select bed_id from bed where bed_number = 'GM 8'));
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for General Ward'),'3','1',(select bed_id from bed where bed_number = 'GM 9'));
+values ((select location_id from location where name = 'General Ward'),'3','1',(select bed_id from bed where bed_number = 'GM 9'));
 
 insert into bed_location_map (location_id ,row_number, column_number, bed_id)
-values ((select location_id from location where name = 'Physical Space for General Ward'),'3','2',(select bed_id from bed where bed_number = 'GM 10'));
+values ((select location_id from location where name = 'General Ward'),'3','2',(select bed_id from bed where bed_number = 'GM 10'));
